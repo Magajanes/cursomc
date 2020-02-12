2b40cb81130f1fb1d8939615e146eb8ccb6bf59c
Magajanes <thiagosmfis@gmail.com>
2020-02-12 00:15:46 -0300

    Create repository and service for Categoria

---
+++ src/main/java/com/magajanes/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.magajanes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.magajanes.cursomc.domain.Categoria;
+import com.magajanes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
